ff72a01c3b30cc4a1ab34fc2da530862522fb7c7
Akhil Raj Arimbra <nilamburakhil@gmail.com>
2018-07-22 16:40:22 +0530

    added UINavigationView

---
+++ Color Magic/ColorPickerVCViewControll.swift
@@ -0,0 +1,35 @@
+//
+//  ColorPickerVC.swift
+//  Color Magic
+//
+//  Created by Akhil Raj Arimbra on 7/22/18.
+//  Copyright © 2018 Akhil Raj Arimbra. All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
